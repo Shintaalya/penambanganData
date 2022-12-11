598a4a5ecf89968818ca49cff72eac73e81ee756
Shintaalya <111713790+Shintaalya@users.noreply.github.com>
2022-12-12 00:26:31 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -14,20 +14,75 @@ from sklearn.tree import DecisionTreeClassifier
 # warnings.filterwarnings("ignore")
 
 
-st.title("Prediksi Demensia")
-st.write("======================================================")
-st.write("Shinta Alya Imani Putri")
-st.write("200411100005")
+st.title("Web Apps - Classification Fruit With Color")
 
-description, upload_data, preprocessing, modeling, implementation = st.tabs(["Description", "Data", "Preprocessing", "Modeling", "Implementation"])
+st.write("================================================================================")
 
-with description:
-    st.write("""# Description """)
-    st.write("Data Set Ini Adalah : Prediksi Demensia")
-    st.write("msosial, atau motivasi. Demenyakit Alzheimer atau stroke.")
+st.write("Name :Shinta Nuriyatul Mahmudiyah")
+st.write("Nim  :200411100135")
+st.write("Grade: Penambangan Data A")
 
-with upload_data:
-    df = pd.read_csv('https://raw.githubusercontent.com/Shintaalya/Datafile/main/citrus.csv')
+data_set_description, data, preprocessing, modeling, implementation = st.tabs(["Data Set Description", "Data", "Preprocessing", "Modeling", "Implementation"])
+
+with data_set_description:
+    st.write("""# Data Set Description """)
+    st.write("###### Data Set Ini Adalah : Fruit with Color ")
+    st.write("###### Sumber Data Set dari Kaggle : https://www.kaggle.com/datasets/mjamilmoughal/fruits-with-colors-dataset")
+    st.write("""Dalam dataset ini terdapat 59 data dan 7 kolom yaitu fruit label, fruit name, fruit subtype, mass width, height, dan color score. Untuk
+     dataset ini mempunyai 4 kelas yaitu Apple, Mandarin, Orange, dan Lemon. 
+   
+    """)
+    st.write("""###### Penjelasan setiap kolom : """)
+    st.write("""1. Fruit Label (Label Buah) :
+    Label Buah ini menrupakan pengganti nama buah. Berikur penjelasan:
+    1. Apel
+    2. Mandarin
+    3. Orange
+    4. Lemon
+   
+    """)
+    st.write("""2. Fruit Name (Nama Buah) :
+    ini akan menjadi outputnya yaitu nama buah.Dalam Aplikasi ini akan nama nuah yang akan diprediksi ada 4 yaitu Apple, Orange, Mandarin, dan Lemon.
+   
+    """)
+    
+    st.write("""3. Fruit Subtype (Tipe Buah) :
+    Ini merupakan tipe buah. untuk buah apel, mandarin, orange, dan lemon mempunyai tipe buah yang berbeda- beda. 
+   
+    """)
+    st.write("""4. Mass (Massa Buah) :
+    setiap buah mempunyai berat dengan satuan gram. setiap buah juga mempunyai massa buah yang berbeda - beda.
+    
+    """)
+    st.write("""5. Width (Lebar Buah):
+    setiap buah mempunyai lebar buah yang berbeda - beda.
+    
+    """)
+    st.write("""6. Height (Tinggi Buah):
+    setiap buah mempunyai tinggi buah yang berbeda - beda.
+    
+    """)
+    st.write("""7. Color_Score (Skor Warna) :
+    setiap buah mempunyai skor warna  yang berbeda - beda.
+    
+    """)
+    st.write("""Dari inputan Massa, Width, Height, dan Color_Score itu akan menghasilkan output nama buah
+    
+    """)
+    
+    st.write("""Memprediksi Nama Buah (output) :
+
+    1. Apple 
+    2. Mandarin 
+    3. Orange 
+    4. Lemon 
+    """)
+    st.write("###### Aplikasi ini untuk : Fruit  Prediction (Prediksi buah) ")
+    st.write("###### Source Code Aplikasi ada di Github anda bisa acces di link : https://github.com/135-ShintaNuriyatulMahmudiyah/PenambanganDataWeb ")
+    st.write("###### Untuk Wa saya anda bisa hubungi nomer ini : http://wa.me/6285704097096 ")
+
+with data:
+    df = pd.read_csv('https://raw.githubusercontent.com/Shintaalya/Datafile/main/citrus.csv)
     st.dataframe(df)
 
 with preprocessing:
@@ -42,8 +97,8 @@ with preprocessing:
     """)
     
     #Mendefinisikan Varible X dan Y
-    X = df[['name','diameter','weight','red','green','blue']]
-    y = df['name'].values
+    X = df[["diameter","weight","red","green","blue"]]
+    y = df["name"].values
     df
     X
     df_min = X.min()
@@ -70,10 +125,10 @@ with preprocessing:
         '2' : [dumies[1]],
         '3' : [dumies[2]],
         '4' : [dumies[3]],
-        '5' : [dumies[4]],
-        
         
     })
+
+    st.write(labels)
 
    
 with modeling:
@@ -102,15 +157,7 @@ with modeling:
         gaussian_akurasi = round(100 * accuracy_score(test_label, y_pred))
         # akurasi = 10
 
-        #Gaussian Naive Bayes
-        # gaussian = GaussianNB()
-        # gaussian = gaussian.fit(training, training_label)
-
-        # probas = gaussian.predict_proba(test)
-        # probas = probas[:,1]
-        # probas = probas.round()
-
-        # gaussian_akurasi = round(100 * accuracy_score(test_label,probas))
+        
 
         #KNN
         K=10
@@ -159,26 +206,20 @@ with modeling:
 with implementation:
     with st.form("my_form"):
         st.subheader("Implementasi")
-        Visit = st.number_input('Masukkan visit (Visit) : ')
-        MR_Delay = st.number_input('Masukkan mr delay (MR Delay) : ')
-        Age = st.number_input('Masukkan age (Age) : ')
-        Educ = st.number_input('Masukkan educ (Educ) : ')
-        MMSE = st.number_input('Masukkan mmse (MMSE) : ')
-        CDR = st.number_input('Masukkan cdr (CDR) : ')
-        eTIV = st.number_input('Masukkan etiv (eTIV) : ')
-        nWBV = st.number_input('Masukkan nwbv (nWBV) : ')
-        ASF = st.number_input('Masukkan asf (ASF) : ')
-        
+        mass = st.number_input('Masukkan berat buah (mass) : ')
+        width = st.number_input('Masukkan lebar buah (width) : ')
+        height = st.number_input('Masukkan tinggi buah (height) : ')
+        color_score = st.number_input('Masukkan skor warna (color_score) : ')
         model = st.selectbox('Pilihlah model yang akan anda gunakan untuk melakukan prediksi?',
                 ('Gaussian Naive Bayes', 'K-NN', 'Decision Tree'))
 
         prediksi = st.form_submit_button("Submit")
         if prediksi:
             inputs = np.array([
-                Weight,
-                Length1,
-                Height,
-                Width
+                mass,
+                width,
+                height,
+                color_score
             ])
 
             df_min = X.min()
@@ -193,7 +234,6 @@ with implementation:
             if model == 'Decision Tree':
                 mod = dt
 
-               
             input_pred = mod.predict(input_norm)
 
 
