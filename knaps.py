a0b429073f7ba1d5fe94220b86fa1abde5c5d992
Shintaalya <111713790+Shintaalya@users.noreply.github.com>
2022-12-12 00:55:44 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -14,73 +14,30 @@ from sklearn.tree import DecisionTreeClassifier
 # warnings.filterwarnings("ignore")
 
 
-st.title("Web Apps - Classification Fruit With Color")
+st.title("Orange vs Grapefruit")
 
 st.write("================================================================================")
 
-st.write("Name :Shinta Alya Imani Putri")
-st.write("Nim  :200411100005")
-st.write("Grade: Penambangan Data A")
+st.write("Shinta Alya Imani Putri")
+st.write("200411100005")
 
-data_set_description, data, preprocessing, modeling, implementation = st.tabs(["Data Set Description", "Data", "Preprocessing", "Modeling", "Implementation"])
+data_set_description, data, preprocessing, modeling, implementation = st.tabs(["Description", "Data", "Preprocessing", "Modeling", "Implementation"])
 
 with data_set_description:
     st.write("""# Data Set Description """)
-    st.write("###### Data Set Ini Adalah : Fruit with Color ")
-    st.write("###### Sumber Data Set dari Kaggle : https://www.kaggle.com/datasets/mjamilmoughal/fruits-with-colors-dataset")
-    st.write("""Dalam dataset ini terdapat 59 data dan 7 kolom yaitu fruit label, fruit name, fruit subtype, mass width, height, dan color score. Untuk
-     dataset ini mempunyai 4 kelas yaitu Apple, Mandarin, Orange, dan Lemon. 
-   
-    """)
-    st.write("""###### Penjelasan setiap kolom : """)
-    st.write("""1. Fruit Label (Label Buah) :
-    Label Buah ini menrupakan pengganti nama buah. Berikur penjelasan:
-    1. Apel
-    2. Mandarin
-    3. Orange
-    4. Lemon
-   
-    """)
-    st.write("""2. Fruit Name (Nama Buah) :
-    ini akan menjadi outputnya yaitu nama buah.Dalam Aplikasi ini akan nama nuah yang akan diprediksi ada 4 yaitu Apple, Orange, Mandarin, dan Lemon.
+    st.write("Data Set Ini Adalah : Klasifikasi Orange vs Grapefruit")
+    st.write("""Tugas memisahkan jeruk dan grapefruit cukup jelas bagi manusia, tetapi bahkan dengan pengamatan manual masih ada sedikit kesalahan. Kumpulan data ini mengambil warna, berat, dan diameter jeruk dan jeruk bali "rata-rata" dan menghasilkan kumpulan data yang lebih besar yang berisi beragam nilai dan merupakan "jeruk" dan "jeruk bali".""")
+    st.write("""Terdapat 6 kolom """)
+    st.write("""Yaitu :
+    1. Name
+    2. Diameter
+    3. Weight
+    4. Red
+    5. Green
+    6. Blue
    
     """)
     
-    st.write("""3. Fruit Subtype (Tipe Buah) :
-    Ini merupakan tipe buah. untuk buah apel, mandarin, orange, dan lemon mempunyai tipe buah yang berbeda- beda. 
-   
-    """)
-    st.write("""4. Mass (Massa Buah) :
-    setiap buah mempunyai berat dengan satuan gram. setiap buah juga mempunyai massa buah yang berbeda - beda.
-    
-    """)
-    st.write("""5. Width (Lebar Buah):
-    setiap buah mempunyai lebar buah yang berbeda - beda.
-    
-    """)
-    st.write("""6. Height (Tinggi Buah):
-    setiap buah mempunyai tinggi buah yang berbeda - beda.
-    
-    """)
-    st.write("""7. Color_Score (Skor Warna) :
-    setiap buah mempunyai skor warna  yang berbeda - beda.
-    
-    """)
-    st.write("""Dari inputan Massa, Width, Height, dan Color_Score itu akan menghasilkan output nama buah
-    
-    """)
-    
-    st.write("""Memprediksi Nama Buah (output) :
-
-    1. Apple 
-    2. Mandarin 
-    3. Orange 
-    4. Lemon 
-    """)
-    st.write("###### Aplikasi ini untuk : Fruit  Prediction (Prediksi buah) ")
-    st.write("###### Source Code Aplikasi ada di Github anda bisa acces di link : https://github.com/135-ShintaNuriyatulMahmudiyah/PenambanganDataWeb ")
-    st.write("###### Untuk Wa saya anda bisa hubungi nomer ini : http://wa.me/6285704097096 ")
-
 with data:
     df = pd.read_csv('https://raw.githubusercontent.com/Shintaalya/Datafile/main/citrus.csv')
     st.dataframe(df)
